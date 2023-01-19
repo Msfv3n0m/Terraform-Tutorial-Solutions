882480ef4b8a68f4753ddbbbab83fb450bcf26c0
Msfv3n0m <71530214+Msfv3n0m@users.noreply.github.com>
2023-01-19 13:26:30 -0500

    Update provider.tf

--- KodeKloud/Terraform Challenges: Solutions/provider.tf
+++ KodeKloud/Terraform Challenges: Solutions/provider.tf
@@ -1,7 +1,23 @@
-terraform = {
-  required_providers = {
+provider "kubernetes" {
+  config_path = "/root/.kube/config"
+}
+resource "webapp-service" "group-name" {
+  spec {
+    port {
+      port = 8080
+      node_port = 30080
+    }
+  type = "NodePort"
+  }
+}
+
+
+terraform {
+  required_version = "1.1.5"
+  required_providers {
     kubernetes = {
       source = "hashicorp/kubernetes"
+      version = "~> 2.11.0"
     }
   }
 }
